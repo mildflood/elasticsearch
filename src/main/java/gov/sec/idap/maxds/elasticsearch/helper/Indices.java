7839d611f4e809ec6966b97fae39c5ca5eb5fd7a
Mildflood <mildflood@users.noreply.github.com>
2021-11-05 01:17:28 -0400

    Add files via upload

--- src/main/java/gov/sec/idap/maxds/elasticsearch/helper/Indices.java
+++ src/main/java/gov/sec/idap/maxds/elasticsearch/helper/Indices.java
@@ -2,6 +2,7 @@ package gov.sec.idap.maxds.elasticsearch.helper;
 
 public final class Indices {
     public static final String ENTITY_INDEX = "entity";
-    public static final String TERMRULE_INDEX = "termrule";
-    public static final String MAXDS_LOOKUP_REFERENCES = "maxds_lookup_references";
+    public static final String TERMRULE_INDEX = "maxds_termrule";
+    public static final String LOOKUP_REFERENCES_INDEX = "maxds_lookup_references";
+    public static final String TERM_MAP_INFO_INDEX = "maxds_term_map_info";
 }
